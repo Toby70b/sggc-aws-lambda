731e6baeda7e5ec872c91a233e8d49d2ff98b562
Toby <tobypl407@googlemail.com>
2021-09-21 13:39:22 +0100

    introducing some constants, adding class for getting games from steam api

--- src/main/java/utils/ResourceFileUtils.java
+++ src/main/java/utils/ResourceFileUtils.java
@@ -9,6 +9,9 @@ import java.io.InputStream;
 
 public class ResourceFileUtils {
 
+    public static final String TEMP_RESOURCE_FILE_NAME = "tempResourceFile";
+    public static final String JSON_FILE_EXTENSION = ".json";
+
     public <T> T deserializeJsonResourceFileIntoObject(String resourceFilePath, Class<T> deserializingClass) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(getResourceFile(resourceFilePath),deserializingClass);
@@ -16,7 +19,7 @@ public class ResourceFileUtils {
 
     private File getResourceFile(String resourceFilePath) throws IOException {
         InputStream inputStream = ResourceFileUtils.class.getResourceAsStream(resourceFilePath);
-        File tempFile = File.createTempFile("tempResourceFile", ".json");
+        File tempFile = File.createTempFile(TEMP_RESOURCE_FILE_NAME, JSON_FILE_EXTENSION);
         FileUtils.copyInputStreamToFile(inputStream, tempFile);
         tempFile.deleteOnExit();
         return tempFile;
---
+++ src/main/java/utils/SteamRequestHandler.java
@@ -0,0 +1,36 @@
+package utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import models.GetAppListResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+@Data
+public class SteamRequestHandler {
+    private static final String URI = "https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=";
+    private static final String STEAM_API_KEY = "B88AF6D15A99EF5A4E01075EF63E5DF2";
+
+    public GetAppListResponse requestAllSteamAppsFromSteamApi() {
+        String getAppListEndpoint = URI + STEAM_API_KEY;
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(getAppListEndpoint);
+            request.addHeader("content-type", "application/json");
+            HttpResponse result = httpClient.execute(request);
+            String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, GetAppListResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
+
